b98a58776dc735b0a851fc145fb7c910100789fc
Pedro <pedrohq238@gmail.com>
2025-01-13 01:16:42 -0300

    Finalizacao atividade

---
+++ index.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("#form")
+function numCompare(numeroUm, numeroDois){
+  if(numeroUm === numeroDois){
+    return false
+  }
+  return numeroDois > numeroUm
+}
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+  let numeroUm = parseFloat(document.querySelector("#num-one").value)
+  let numeroDois = parseFloat(document.querySelector("#num-two").value)
+  const mensagem = numCompare(numeroUm, numeroDois) === true ? "O número do campo B é maior! Positivo" : "O número do campo A é maior ou igual ao campo B! Negativo"
+  alert(mensagem)
+})
