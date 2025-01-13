51f28da9e1e10a5bead3f6bea45242d6e2da2580
Pedro <pedrohq238@gmail.com>
2025-01-13 01:21:17 -0300

    Melhoramentes codigo

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const form = document.querySelector("#form")
 function numCompare(numeroUm, numeroDois){
-  if(numeroUm === numeroDois){
-    return false
-  }
   return numeroDois > numeroUm
 }
 form.addEventListener('submit', function(e){
   e.preventDefault()
-  let numeroUm = parseFloat(document.querySelector("#num-one").value)
-  let numeroDois = parseFloat(document.querySelector("#num-two").value)
+  let [numeroUm, numeroDois] = [parseFloat(document.querySelector("#num-one").value), parseFloat(document.querySelector("#num-two").value)]
   const mensagem = numCompare(numeroUm, numeroDois) === true ? "O número do campo B é maior! Positivo" : "O número do campo A é maior ou igual ao campo B! Negativo"
   alert(mensagem)
 })
